3bb565e4a1c8cd5ee3b003018245f1c77318e40b
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2024-07-18 23:23:38 +0100

    Added tests

---
+++ test/error.test.ts
@@ -0,0 +1,32 @@
+import { FastifyInstance } from 'fastify';
+import { assert } from 'chai';
+
+import { call, server } from './utils';
+
+suite('error', () => {
+  let app: FastifyInstance;
+
+  suiteSetup(async () => {
+    app = await server();
+  });
+
+  suiteTeardown(async () => {
+    await app.close();
+  });
+
+  test('unhandled routes return 404', async () => {
+    const response = await call(app, '/unhandled-route');
+
+    assert.equal(response.statusCode, 404);
+
+    assert.equal(
+      response.headers['content-type'],
+      'application/json; charset=utf-8',
+    );
+
+    const data = response.json();
+
+    assert.equal(data.error, 'Not Found');
+    assert.equal(data.statusCode, 404);
+  });
+});

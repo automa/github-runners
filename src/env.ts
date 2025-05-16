0cc9833d1bba406038e20b665307c638c644fd1f
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2025-05-16 16:01:19 +0100

    Change product and service consts

--- src/env.ts
+++ src/env.ts
@@ -13,8 +13,8 @@ export const environment = process.env.NODE_ENV || 'development';
 export const isTest = environment === 'test';
 export const isProduction = !isTest && environment !== 'development';
 
-export const product = 'github-runners';
-export const service = 'bot';
+export const product = 'bots';
+export const service = 'github-runners';
 export const version = pkg.version;
 
 const schema = Type.Object({
